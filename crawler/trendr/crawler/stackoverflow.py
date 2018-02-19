17eee280e9856b54d2260ed2fd1f6540a102920c
Jose Asuncion <jeunito@gmail.com>
2018-02-19 00:25:58 -0800

    get tags instead

--- crawler/trendr/crawler/stackoverflow.py
+++ crawler/trendr/crawler/stackoverflow.py
@@ -4,7 +4,7 @@ def crawl():
     stackoverflow = feedparser.parse("https://stackoverflow.com/jobs/feed?q=devops")
     entries = []
     for raw_entry in stackoverflow.entries:
-        entry = (raw_entry.title, raw_entry.description)
+        entry = (raw_entry.title, map(lambda i: str(i["term"]), raw_entry.tags))
         entries.append(entry)
 
     return entries
