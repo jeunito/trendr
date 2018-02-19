2035f302999f561b507805493fad4fab85c39b98
Jose Asuncion <jeunito@gmail.com>
2018-02-19 01:10:03 -0800

    have stdout/non stdout mode

--- crawler/trendr/crawler/__init__.py
+++ crawler/trendr/crawler/__init__.py
@@ -1,7 +1,27 @@
 from . import stackoverflow
 
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument("-s", "--stdout", help="Print crawl data to stdout", action="store_true")
+args = parser.parse_args()
+
+if not args.stdout:
+    import json
+    import hashlib
+    m = hashlib.md5()
+
 def main():
     entries = stackoverflow.crawl()
     for entry in entries:
-        title, _ = entry
-        print title
+        title, tags = entry
+        if args.stdout:
+            print title
+            print "--- {tags}".format(tags=",".join(tags))
+        else:
+            m = hashlib.md5()
+            m.update(entry[0])
+            entry_file = "/var/tmp/crawler/{entry_filename}.json".format(entry_filename=m.hexdigest())
+
+            with open(entry_file, "w") as f:
+                f.write(json.dumps(entry))
